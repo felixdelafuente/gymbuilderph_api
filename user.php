9a5f5c5ca1615b4897a30d040e4c5a4a34192e3b
Felix Jerome Dela Fuente <dlfuente.felixjerome@gmail.com>
2023-09-08 10:19:02 +0800

    fix: update user api

--- user.php
+++ user.php
@@ -3,9 +3,6 @@
 // Include the database connection code
 include 'db.php';
 
-// Define a secret key for encryption and decryption
-$key = 'secret key';
-
 // Get the request method from the server
 $method = $_SERVER['REQUEST_METHOD'];
 
@@ -21,26 +18,20 @@ switch ($method) {
       $stmt = $pdo->prepare($sql);
       $stmt->execute(['user_id' => $user_id]);
       $user = $stmt->fetch(PDO::FETCH_ASSOC);
-      // Decrypt the password using openssl_decrypt()
-      $user['password'] = openssl_decrypt($user['password'], 'AES-128-ECB', $key);
+      // No need to decrypt the password as it is already hashed by password_hash()
     } else {
       // Otherwise, get all users data
       $sql = "SELECT * FROM users";
       $stmt = $pdo->query($sql);
       $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
-      // Decrypt the passwords using openssl_decrypt()
-      foreach ($users as &$user) {
-        $user['password'] = openssl_decrypt($user['password'], 'AES-128-ECB', $key);
-      }
+      // No need to decrypt the passwords as they are already hashed by password_hash()
     }
     break;
   case 'PUT':
-    // Parse the input data from php://input
-    parse_str(file_get_contents('php://input'), $input);
-    // Encrypt the password using openssl_encrypt()
-    $input['password'] = openssl_encrypt($input['password'], 'AES-128-ECB', $key);
+    // Parse the input data from php://input as json
+    $input = json_decode(file_get_contents('php://input'), true);
     // Update the user data by user_id
-    $sql = "UPDATE users SET first_name = :first_name, last_name = :last_name, email = :email, password = :password WHERE user_id = :user_id";
+    $sql = "UPDATE users SET first_name = :first_name, last_name = :last_name, email = :email WHERE user_id = :user_id";
     $stmt = $pdo->prepare($sql);
     $stmt->execute($input);
     break;
@@ -58,11 +49,11 @@ header('Content-Type: application/json');
 // Encode the data as a JSON string and send it as a response
 if ($method == 'GET') {
   if ($user_id) {
-    echo json_encode($user); // Send the user data for the given user_id
+    echo json_encode([$user]); // Send the user data for the given user_id as a list/array of one element
   } else {
-    echo json_encode($users); // Send all users data
+    echo json_encode($users); // Send all users data as a list/array of multiple elements
   }
 } else {
-  echo json_encode(['status' => 'success']); // Send a success message for other methods
+  echo json_encode(['status' => 'success']); // Send a success message as a json object
 }
 ?>
